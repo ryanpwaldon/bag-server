c987fefcf2cf48a9b27c00a2feaa6fbf3beaf054
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-04 14:59:14 +1100

    fix: updates

--- src/modules/cross-sell/cross-sell.module.ts
+++ src/modules/cross-sell/cross-sell.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common'
 import { CrossSellController } from './cross-sell.controller'
 import { CrossSellService } from './cross-sell.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { CrossSellSchema, CrossSell } from './schema/cross-sell.schema'
+import { UserModule } from '../user/user.module'
 
 @Module({
+  imports: [
+    UserModule,
+    MongooseModule.forFeature([
+      {
+        name: CrossSell.name,
+        schema: CrossSellSchema
+      }
+    ])
+  ],
   controllers: [CrossSellController],
   providers: [CrossSellService]
 })
