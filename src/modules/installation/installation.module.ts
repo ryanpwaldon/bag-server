b4ea7bd09faed03ac97137c76115deb2e1647419
Ryan Waldon <ryanpwaldon@gmail.com>
2020-12-30 13:20:36 +1100

    chore: clean

--- src/modules/installation/installation.module.ts
+++ src/modules/installation/installation.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common'
-import { InstallationService } from './installation.service'
-import { InstallationController } from './installation.controller'
-import { AdminModule } from '../admin/admin.module'
 import { UserModule } from '../user/user.module'
+import { AdminModule } from '../admin/admin.module'
 import { AppUrlModule } from '../app-url/app-url.module'
 import { WebhookModule } from '../webhook/webhook.module'
+import { InstallationService } from './installation.service'
 import { ScriptTagModule } from '../script-tag/script-tag.module'
+import { InstallationController } from './installation.controller'
 import { SubscriptionModule } from '../subscription/subscription.module'
 import { CartModule } from 'src/modules/cart/cart.module'
 
 @Module({
-  providers: [InstallationService],
-  controllers: [InstallationController],
   imports: [AdminModule, UserModule, AppUrlModule, WebhookModule, ScriptTagModule, SubscriptionModule, CartModule],
+  controllers: [InstallationController],
+  providers: [InstallationService],
   exports: [InstallationService]
 })
 export class InstallationModule {}
