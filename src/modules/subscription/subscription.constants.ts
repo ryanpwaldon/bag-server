3db983793c4a42b78cd2dbeccd26a28b6f6b4191
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-04 15:14:44 +1100

    chore: rename

--- src/modules/subscription/subscription.constants.ts
+++ src/modules/subscription/subscription.constants.ts
@@ -6,7 +6,7 @@ export const PAID_SUBSCRIPTION_CREATED_PATH = 'paid-subscription-created'
 
 const SUBSCRIPTIONS: Subscription[] = [
   {
-    name: 'Starter V1',
+    name: 'Starter (1)',
     price: 0,
     trialDays: 0,
     interval: Interval.Monthly,
@@ -19,7 +19,7 @@ const SUBSCRIPTIONS: Subscription[] = [
     legacy: false
   },
   {
-    name: 'Growth V1',
+    name: 'Growth (1)',
     price: 98,
     trialDays: 30,
     interval: Interval.Monthly,
