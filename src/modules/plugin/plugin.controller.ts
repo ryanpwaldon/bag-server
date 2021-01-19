5e499171e1c2943e04c078a434125a5331f0f761
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-19 20:21:44 +1100

    feat: update plugin controller

--- src/modules/plugin/plugin.controller.ts
+++ src/modules/plugin/plugin.controller.ts
@@ -5,24 +5,14 @@ import { PluginGuard } from 'src/common/guards/plugin.guard'
 import { User as UserType } from 'src/modules/user/schema/user.schema'
 import { BadRequestException, Controller, Get, UseGuards } from '@nestjs/common'
 
-type PluginSettings = {
-  active: boolean
-  permissions: Permission[]
-}
-
 @Controller('plugin')
 export class PluginController {
   constructor(private readonly cartService: CartService) {}
 
-  @Get()
+  @Get('permissions')
   @UseGuards(PluginGuard)
-  async findSettings(@User() user: UserType): Promise<PluginSettings> {
+  async findSettings(@User() user: UserType): Promise<Permission[]> {
     if (!user) throw new BadRequestException()
-    const cart = await this.cartService.findOneByUserId(user.id as string)
-    if (!cart) throw new BadRequestException()
-    return {
-      active: cart.active,
-      permissions: user.permissions
-    }
+    return user.permissions
   }
 }
