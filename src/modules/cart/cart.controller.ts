340763936cbaf37f59d87303df2e1f3b56bcc7ad
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-19 14:47:36 +1100

    chore: update guard

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -3,14 +3,13 @@ import { User } from 'src/common/decorators/user.decorator'
 import { Cart } from 'src/modules/cart/schema/cart.schema'
 import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common'
 import { EmbeddedAppGuard } from 'src/common/guards/embedded-app.guard'
-import { EmbeddedAppOrPluginGuard } from 'src/common/guards/embedded-app-or-plugin.guard'
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Get()
-  @UseGuards(EmbeddedAppOrPluginGuard)
+  @UseGuards(EmbeddedAppGuard)
   async findOneByUserId(@User('id') userId: string) {
     return this.cartService.findOneByUserId(userId)
   }
