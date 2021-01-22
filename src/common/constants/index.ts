cee29971437b8b486eca988abbc24f0c52be7be7
Ryan Waldon <ryanpwaldon@gmail.com>
2021-01-22 16:55:45 +1100

    chore: move shopify api version to constants

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,1 +1,2 @@
 export const SENTRY_DSN = 'https://b9858d3108f944a29730ae5957eba947@o352508.ingest.sentry.io/5570262'
+export const SHOPIFY_API_VERSION = '2020-07'
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,8 +1,8 @@
 import { Request } from 'express'
 import { Logger } from 'nestjs-pino'
 import { REQUEST } from '@nestjs/core'
-import { ConfigService } from '@nestjs/config'
 import { User } from 'src/modules/user/schema/user.schema'
+import { SHOPIFY_API_VERSION } from 'src/common/constants'
 import { Injectable, HttpService, Scope, Inject, InternalServerErrorException } from '@nestjs/common'
 
 @Injectable({ scope: Scope.REQUEST })
@@ -10,14 +10,13 @@ export class AdminService {
   constructor(
     private readonly logger: Logger,
     private readonly httpService: HttpService,
-    private readonly configService: ConfigService,
     @Inject(REQUEST) private request: Request & { user: User }
   ) {}
 
   async createGraphQLRequest(data: any) {
     const method = 'post'
     const { shopOrigin, accessToken } = this.request.user
-    const url = `https://${shopOrigin}/admin/api/${this.configService.get('SHOPIFY_API_VERSION')}/graphql.json`
+    const url = `https://${shopOrigin}/admin/api/${SHOPIFY_API_VERSION}/graphql.json`
     const headers = { 'Content-Type': 'application/json', 'X-Shopify-Access-Token': accessToken }
     const response = await this.httpService.request({ method, headers, url, data }).toPromise()
     if (response.data.errors) {
@@ -29,7 +28,7 @@ export class AdminService {
 
   async createRestRequest(method: 'post' | 'get' | 'put', path: string, data?: any) {
     const { shopOrigin, accessToken } = this.request.user
-    const url = `https://${shopOrigin}/admin/api/${this.configService.get('SHOPIFY_API_VERSION')}/${path}`
+    const url = `https://${shopOrigin}/admin/api/${SHOPIFY_API_VERSION}/${path}`
     const headers = { 'Content-Type': 'application/json', 'X-Shopify-Access-Token': accessToken }
     const response = await this.httpService.request({ method, headers, url, data }).toPromise()
     if (response.data.errors) {
