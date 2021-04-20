fd68cde98f317e1c20257aaa0f7f1d8c1c12dfe6
Ryan Waldon <ryanpwaldon@gmail.com>
2021-04-20 23:36:16 +0930

    chore: cleanup

--- src/modules/migration/migration.module.ts
+++ src/modules/migration/migration.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common'
 import { MigrationService } from './migration.service'
-import { EventModule } from 'src/modules/event/event.module'
-import { OrderModule } from 'src/modules/order/order.module'
-import { ConversionModule } from 'src/modules/conversion/conversion.module'
 
 @Module({
-  imports: [EventModule, OrderModule, ConversionModule],
+  imports: [],
   providers: [MigrationService]
 })
 export class MigrationModule {}
--- src/modules/migration/migration.service.ts
+++ src/modules/migration/migration.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import { OrderService } from 'src/modules/order/order.service'
-import { ConversionService } from 'src/modules/conversion/conversion.service'
 
 @Injectable()
 export class MigrationService {
-  constructor(private readonly orderService: OrderService, private readonly conversionService: ConversionService) {}
-
+  // @Timeout(1000)
   async migrate() {
-    // console.log('start')
+    // console.log('Start')
     // try {
-    //   const conversions = await this.conversionService.findAll()
-    //   for (const conversion of conversions) {
-    //     const data = {
-    //       user: conversion.user,
-    //       details: conversion.order as any,
-    //       createdAt: (conversion as any).createdAt
-    //     } as LeanDocument<Order>
-    //     const order = await this.orderService.create(data)
-    //     conversion.order = Types.ObjectId(order.id) as any
-    //     await conversion.save()
-    //   }
-    //   console.log('finished')
+    //   const items = await...
+    //   console.log('Docs count: ' + '')
+    //   const itemsToUpdate = []
+    //   await Promise.all(itemsToUpdate)
+    //   console.log('Docs updated: ' + '')
+    //   console.log('End')
     // } catch (err) {
     //   console.log(err)
     // }
