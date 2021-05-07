0220acc2338a3b8cb9bef8b2c614b0ec85b3635a
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-07 19:05:29 +0930

    chore: cleanup

--- src/modules/test/test.service.ts
+++ src/modules/test/test.service.ts
@@ -1,4 +1,3 @@
-import { Timeout } from '@nestjs/schedule'
 import { Injectable } from '@nestjs/common'
 import { UserService } from 'src/modules/user/user.service'
 import { SalesService } from 'src/modules/sales/sales.service'
@@ -13,28 +12,4 @@ export class TestService {
     private readonly shopDetailsService: ShopDetailsService,
     private readonly subscriptionService: SubscriptionService
   ) {}
-
-  // @Timeout(1000)
-  async start() {
-    const users = await this.userService.findAll({ uninstalled: false })
-    console.log('Count: ' + users.length)
-    for (const user of users) {
-      try {
-        console.log('User: ' + user.shopOrigin)
-        const shopDetails = await this.shopDetailsService.find(user)
-        user.email = shopDetails.email
-        user.appUrl = shopDetails.appUrl
-        user.timezone = shopDetails.timezone
-        user.storeName = shopDetails.storeName
-        user.shopifyPlan = shopDetails.shopifyPlan
-        user.shopifyPlus = shopDetails.shopifyPlus
-        user.currencyCode = shopDetails.currencyCode
-        user.primaryDomain = shopDetails.primaryDomain
-        user.developmentStore = shopDetails.developmentStore
-        await user.save()
-      } catch (err) {
-        console.log('An error occurred')
-      }
-    }
-  }
 }
