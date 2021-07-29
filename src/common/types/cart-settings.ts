3c806a10e2f5c453ee01259ca323924912f2f577
Ryan Waldon <ryanpwaldon@gmail.com>
2021-07-29 16:24:12 +1000

    feat: theme customization updates

--- src/common/types/cart-settings.ts
+++ src/common/types/cart-settings.ts
@@ -3,57 +3,57 @@ import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator'
 export class CartSettings {
   @IsOptional()
   @IsString()
-  colorBackdrop = '#000000'
-
-  @IsOptional()
-  @IsString()
-  colorBackground = '#F5F5F5'
-
-  @IsOptional()
-  @IsString()
-  colorBorderPrimary = '#E5E5E5'
-
-  @IsOptional()
-  @IsString()
-  colorButtonPrimaryBackground = '#171717'
-
-  @IsOptional()
-  @IsString()
-  colorButtonPrimaryText = '#FFFFFF'
-
-  @IsOptional()
-  @IsString()
-  colorProgressBarPrimary = '#404040'
-
-  @IsOptional()
-  @IsString()
-  colorTextLink = '#2563EB'
-
-  @IsOptional()
-  @IsString()
-  colorTextPrimary = '#171717'
-
-  @IsOptional()
-  @IsString()
-  colorTextSecondary = '#737373'
-
-  @IsOptional()
-  @IsBoolean()
-  displayBorders = false
-
-  @IsOptional()
-  @IsBoolean()
-  displayShadows = true
+  backdropColor = '#000000'
 
   @IsOptional()
   @IsNumber()
-  opacityBackdrop = 0.5
+  backdropOpacity = 0.5
+
+  @IsOptional()
+  @IsString()
+  borderColor1 = '#E5E5E5'
 
   @IsOptional()
   @IsNumber()
-  paddingCart = 0
+  brightness1 = 0.1
+
+  @IsOptional()
+  @IsNumber()
+  cartPadding = 0
 
   @IsOptional()
   @IsBoolean()
-  roundedCorners = true
+  roundCorners = true
+
+  @IsOptional()
+  @IsBoolean()
+  showBorders = false
+
+  @IsOptional()
+  @IsBoolean()
+  showShadows = true
+
+  @IsOptional()
+  @IsString()
+  textColor1 = '#171717'
+
+  @IsOptional()
+  @IsString()
+  textColor2 = '#737373'
+
+  @IsOptional()
+  @IsString()
+  textColor3 = '#ffffff'
+
+  @IsOptional()
+  @IsString()
+  themeColor1 = '#F5F5F5'
+
+  @IsOptional()
+  @IsString()
+  themeColor2 = '#ffffff'
+
+  @IsOptional()
+  @IsString()
+  themeColor3 = '#2563EB'
 }
