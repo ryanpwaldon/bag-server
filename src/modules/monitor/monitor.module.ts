fbaa6e09fa8c83055d7c22e85f06788774d41f41
Ryan Waldon <ryanpwaldon@gmail.com>
2021-08-08 12:31:41 +1000

    clean: remove monitor service

--- src/modules/monitor/monitor.module.ts
+++ src/modules/monitor/monitor.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
-import { MonitorService } from './monitor.service'
 import { MonitorController } from './monitor.controller'
 
 @Module({
-  providers: [MonitorService],
   controllers: [MonitorController]
 })
 export class MonitorModule {}
--- src/modules/monitor/monitor.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-
-@Injectable()
-export class MonitorService {
-  constructor(readonly configService: ConfigService) {}
-}
